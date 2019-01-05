349430692ac943c3e08db3cca845e840c0a1b6c5
MisunJoo <0987someday@naver.com>
2019-01-05 17:50:21 +0900

    Add : .gitignore, health-check controller 추가

---
+++ src/main/java/org/mashup/pream/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package org.mashup.pream.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HealthCheckController {
+  @GetMapping(value = "/health-check")
+  public String healthCheck() {
+    log.info("190105 logTest1");
+
+    return "pream server is running now.";
+  }
+
+}
