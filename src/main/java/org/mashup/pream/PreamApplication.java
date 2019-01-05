ca328a9513741641486325fbe2fb51ef7f6e5ca2
MisunJoo <0987someday@naver.com>
2019-01-05 17:44:24 +0900

    First Commit

---
+++ src/main/java/org/mashup/pream/PreamApplication.java
@@ -0,0 +1,14 @@
+package org.mashup.pream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PreamApplication.class, args);
+  }
+
+}
+
