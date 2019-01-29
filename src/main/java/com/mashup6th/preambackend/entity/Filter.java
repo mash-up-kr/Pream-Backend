288376295ac793501bd99bf6eff9d54431684bcf
leesoo <isoo7510@gmail.com>
2019-01-30 00:14:37 +0900

    2019.01.29 - MODIFY: userfilter, user modeling (add)

--- src/main/java/com/mashup6th/preambackend/entity/Filter.java
+++ src/main/java/com/mashup6th/preambackend/entity/Filter.java
@@ -18,32 +18,77 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Setter
 @Entity
 public class Filter {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(length = 50, nullable = false)
-  private String name;
+    @Column(length = 50, nullable = false)
+    private String name;
 
-  @Column(nullable = false)
-  @CreationTimestamp
-  private LocalDateTime regDate;
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime regDate;
 
-  @Column
-  @UpdateTimestamp
-  private LocalDateTime updateDate;
+    @Column
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
 
-  @Column(columnDefinition = "boolean default false")
-  private Boolean sharedYn;
+    @Column(columnDefinition = "boolean default false")
+    private Boolean sharedYn;
 
-  //어떤 유저가 이 filter를 생성했는지에 대한 정보
-  @OneToMany
-  @JoinColumn(name = "user_id")
-  private Set<User> users;
+    @Column(nullable = false)
+    private int sharedCount;
 
-  @OneToMany(mappedBy = "filter")
-  private Set<UserFilter> userFilters;
+    @Column
+    private Float exposure;
 
-  @OneToMany(mappedBy = "filter")
-  private Set<FilterCategory> filterCategories;
+    @Column
+    private Float contrast;
+
+    @Column
+    private Float adjust;
+
+    @Column
+    private Float sharpen;
+
+    @Column
+    private Float clarity;
+
+    @Column
+    private Float saturation;
+
+    @Column
+    private Float tone;
+
+    @Column
+    private Float whiteBalance;
+
+    @Column
+    private Float vignette;
+
+    @Column
+    private Float grain;
+
+    @Column
+    private Float fade;
+
+    @Column
+    private Float splitTone;
+
+    @Column
+    private Float colorFilter;
+
+    //어떤 유저가 이 filter를 생성했는지에 대한 정보
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private Set<User> users;
+
+    @OneToMany(mappedBy = "filter")
+    private Set<UserFilter> userFilters;
+
+    @OneToMany(mappedBy = "filter")
+    private Set<FilterCategory> filterCategories;
+
+
+
 }
