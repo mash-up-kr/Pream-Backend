39447d65f46bdefc4a7b92d77c45448f9a534bf2
leesoo <isoo7510@gmail.com>
2019-02-05 18:36:46 +0900

    2019.02.05 - add: list filter findAll feature

--- src/main/java/com/mashup6th/preambackend/persistence/MyFilterRepository.java
+++ src/main/java/com/mashup6th/preambackend/persistence/MyFilterRepository.java
@@ -12,5 +12,6 @@ public interface MyFilterRepository extends JpaRepository<Filter, Long> {
     Filter findByName(String name);
     Filter findByCategory(List<String> category);
     Page<Filter> findByAll(Pageable pageable);
+    List<Filter> findAll(Filter filter);
 
 }
