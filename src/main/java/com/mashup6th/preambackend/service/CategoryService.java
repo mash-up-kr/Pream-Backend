9c05db6907a1fa070d7f886f8cbd6c91417762d2
MisunJoo <0987someday@naver.com>
2019-02-13 20:22:42 +0900

    2019.02.13 - Fix : Entity 외래키 끊어줌

--- src/main/java/com/mashup6th/preambackend/service/CategoryService.java
+++ src/main/java/com/mashup6th/preambackend/service/CategoryService.java
@@ -1,12 +1,12 @@
-package com.mashup6th.preambackend.service;
-
-import com.mashup6th.preambackend.dto.category.CategoryInfo;
-import com.mashup6th.preambackend.entity.Category;
-import java.util.List;
-
-public interface CategoryService {
-  Boolean save(CategoryInfo categoryInfo);
-  CategoryInfo modify(CategoryInfo categoryInfo);
-  Boolean delete(Long userId, Long categoryId);
-  List<Category> getCategory(Long userId);
-}
+//package com.mashup6th.preambackend.service;
+//
+//import com.mashup6th.preambackend.dto.category.CategoryInfo;
+//import com.mashup6th.preambackend.entity.Category;
+//import java.util.List;
+//
+//public interface CategoryService {
+//  Boolean save(CategoryInfo categoryInfo);
+//  CategoryInfo modify(CategoryInfo categoryInfo);
+//  Boolean delete(Long userId, Long categoryId);
+//  List<Category> getCategory(Long userId);
+//}
