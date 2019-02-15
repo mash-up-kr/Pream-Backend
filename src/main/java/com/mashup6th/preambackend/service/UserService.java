ece5668c6543bc34470ae5e7ba2d9e730d355d0b
MisunJoo <0987someday@naver.com>
2019-02-15 19:05:25 +0900

    Fix : 사용자가 로그인 하였을 때 nickname을 클라이언트에게 제공, ApiResponses 등록

--- src/main/java/com/mashup6th/preambackend/service/UserService.java
+++ src/main/java/com/mashup6th/preambackend/service/UserService.java
@@ -1,14 +1,15 @@
-//package com.mashup6th.preambackend.service;
-//
-//import com.mashup6th.preambackend.dto.SignUpJson;
-//import com.mashup6th.preambackend.dto.user.UserLoginInfo;
-//import com.mashup6th.preambackend.entity.User;
-//
-//public interface UserService {
-//  public String emailCheck(String email);
-//  public boolean nicknameCheck(String email);
-//  public void save(SignUpJson signUpJson);
-//  public User login(UserLoginInfo userLoginInfo);
-//  public String sendEmail(String email);
-//
-//  }
+package com.mashup6th.preambackend.service;
+
+import com.mashup6th.preambackend.dto.SignUpJson;
+import com.mashup6th.preambackend.dto.user.UserLoginInfo;
+import com.mashup6th.preambackend.entity.User;
+
+public interface UserService {
+  public String emailCheck(String email);
+  public boolean nicknameCheck(String email);
+  public void save(SignUpJson signUpJson);
+  public User login(UserLoginInfo userLoginInfo);
+  public String sendEmail(String email);
+  public String getUserNickname(String email);
+
+  }
