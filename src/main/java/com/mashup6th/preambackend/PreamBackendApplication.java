24a7ee2dec2639112bdc7fec58e8477eb45a26f1
leesoo <isoo7510@gmail.com>
2019-02-19 17:45:07 +0900

    2019.02.19 - fileController debug..

--- src/main/java/com/mashup6th/preambackend/PreamBackendApplication.java
+++ src/main/java/com/mashup6th/preambackend/PreamBackendApplication.java
@@ -2,6 +2,10 @@ package com.mashup6th.preambackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class PreamBackendApplication {
@@ -10,5 +14,11 @@ public class PreamBackendApplication {
         SpringApplication.run(PreamBackendApplication.class, args);
     }
 
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("100Mb");
+        return factory.createMultipartConfig();
+    }
 }
 
